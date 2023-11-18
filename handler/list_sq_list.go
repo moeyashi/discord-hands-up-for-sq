ed0f31edb82f073430a3b80366c253ee71acd883
Ren Adachi <49895682+moeyashi@users.noreply.github.com>
2023-11-18 16:54:02 +0900

    feat(list_sq_list): button表示するように

--- handler/list_sq_list.go
+++ handler/list_sq_list.go
@@ -15,24 +15,50 @@ func ListSQ(ctx context.Context, s *discordgo.Session, i *discordgo.InteractionC
 		s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
 			Type: discordgo.InteractionResponseChannelMessageWithSource,
 			Data: &discordgo.InteractionResponseData{
-				Flags: discordgo.MessageFlagsEphemeral,
+				Flags:   discordgo.MessageFlagsEphemeral,
 				Content: fmt.Sprint(err),
 			},
 		})
 		return
 	}
-	content := "SQが登録されていません。"
-	if len(guild.SQList) > 0 {
-		content = ""
-		for _, v := range guild.SQList {
-			content += v.Title + "\n"
+	if len(guild.SQList) == 0 {
+		s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+			Type: discordgo.InteractionResponseChannelMessageWithSource,
+			Data: &discordgo.InteractionResponseData{
+				Flags:   discordgo.MessageFlagsEphemeral,
+				Content: "SQが登録されていません。",
+			},
+		})
+		return
+	}
+
+	embedFields := []*discordgo.MessageEmbedField{}
+	components := []discordgo.MessageComponent{}
+	for _, sq := range guild.SQList {
+		embedFields = append(embedFields, &discordgo.MessageEmbedField{
+			Name:  sq.Title,
+			Value: "なし",
+		})
+		if len(components) < 5 {
+			components = append(components, discordgo.Button{
+				CustomID: "button_" + sq.Title,
+				Label:    sq.Title,
+				Style:    discordgo.DangerButton,
+			})
 		}
 	}
-	s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+	if err := s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
 		Type: discordgo.InteractionResponseChannelMessageWithSource,
 		Data: &discordgo.InteractionResponseData{
-			Flags: discordgo.MessageFlagsEphemeral,
-			Content: content,
+			Flags:  discordgo.MessageFlagsEphemeral,
+			Embeds: []*discordgo.MessageEmbed{{Fields: embedFields}},
+			Components: []discordgo.MessageComponent{
+				discordgo.ActionsRow{
+					Components: components,
+				},
+			},
 		},
-	})
+	}); err != nil {
+		fmt.Println(err)
+	}
 }
